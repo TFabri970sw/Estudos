3826bb00ddedc90ee947adc565cb709216461cb1
TFabri970sw <154623272+TFabri970sw@users.noreply.github.com>
2024-01-11 10:43:09 -0300

    Add files via upload

---
+++ aula7.py
@@ -0,0 +1,18 @@
+# Variaveis são usadas para salvar algo na memoria do coputador.
+# PEP8: inicie variaveis com letras minusculas, pode usar numeros e underline_.
+# o sinal de = e o operador de atribuição. Ele e usado para atribuir um valor a um nome (variavel).
+# uso: nome_variavel = expressão
+nome_completo = 'Thiago Fabri'
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois) 
+print(int('1'), type(int('1')))
+int_um = int('1')
+print(int_um, type(int_um))
+int_um = bool('1')
+print(int_um, type(int_um))
+
+nome = 'Thiago'
+idade = 16
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('E maior?', maior_de_idade)
---
+++ aula8.py
@@ -0,0 +1,13 @@
+Nome = 'Thiago'
+Sobrenome = 'Fabri'
+Idade = 16
+Ano_de_nacimento = 2024 - Idade
+maior_de_idade = Idade >= 18
+altura_metros = 1.70
+
+print('Nome:', Nome)
+print('Sobrenome:', Sobrenome)
+print('Idade:', Idade)
+print('Ano de nacimento:', Ano_de_nacimento)
+print('E maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
