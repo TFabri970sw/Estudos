78cf7a053974bbe8e60660a77fa074e91b7edc6a
TFabri970sw <154623272+TFabri970sw@users.noreply.github.com>
2024-03-27 14:27:01 -0300

    Add files via upload

---
+++ aula15.py
@@ -0,0 +1,11 @@
+#nome = input('Qual e o seu nome? ') 
+ 
+#print(f'O seu nome e {nome=}')
+
+numero_1 = int(input('Digite um numero: '))
+numero_2 = int(input('Digite outro numero: '))
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print (f'A soma dos numeros e: {numero_1 + numero_2}')
---
+++ aula16.py
@@ -0,0 +1,20 @@
+# if / elif      / else
+# se / se não se / se não
+entrada = input('Voce quer "entrar" ou "sair"?')
+
+if entrada == 'entrar':
+    print('voce entrou no sistema')
+    print('Bem vindo')
+    print('Eu so uma ia capaz de mudar o mundo')
+    print('O que posso fazer pro voce hoje?')
+
+elif entrada == 'sair':
+    print('Voce saiu do sistema')
+    print('não entendi')
+    print('Porque saiu?')
+    print('Verme')
+else:
+    print('Voce não digitou nem entrar e nem sair.')
+    print('????')
+    print('não sabe escrever?')
+    print('e um verme mesmo')
