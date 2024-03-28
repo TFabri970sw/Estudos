2236acbd7040f156a92cf13040266d649f43449c
TFabri970sw <154623272+TFabri970sw@users.noreply.github.com>
2024-03-28 10:59:25 -0300

    Add files via upload

---
+++ aula18.py
@@ -0,0 +1,33 @@
+# if / elif      / else
+# se / se não se / se não
+
+Condicão1 = False
+Condicão2 = False
+Condicão3 = True
+Condicão4 = False
+
+if Condicão1:
+    print('codigo para condição 1')
+    pass
+
+elif Condicão2:
+    print('codigo para a condição 2')
+    pass
+
+elif Condicão3:
+    print('codigo para a condição 3')
+    pass
+
+elif Condicão4:
+    print('codigo para a condição 4')
+    pass
+
+else:
+    print('Nenhuma condição foi satisfeita')
+    pass
+
+if 10 == 10:
+    print('outro if')
+    pass
+
+print('Fora do if')
---
+++ aula19.py
@@ -0,0 +1,24 @@
+"""
+Operadores de comparação (relacionais)
+OP          Significado         Exemplo (true)
+>           Maoir               2 < 1
+>=          maior ou igual      2 >= 2
+<           menor               1 < 2
+<=          menor ou igual      2 <= 2
+==          igual               'a' == 'a'
+!=          diferente           'a' != 'b'
+"""
+maior = 2 > 1
+maoir_ou_igual = 2 >= 2
+menor = 1 < 2 
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+pass
+
+print (maior)
+print(maoir_ou_igual)
+print(menor)
+print(menor_ou_igual)
+print(igual)
+print(diferente)
